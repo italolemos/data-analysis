0c9a53393f289cc2d93dec13d777cb15f9f7e208
Ítalo Lemos <italo_vinicius13@yahoo.com.br>
2019-05-02 23:29:20 -0300

    first commit

---
+++ lista.R
@@ -0,0 +1,28 @@
+setwd("/Users/italolemos/Documents/Meu RCurso")
+
+# 3
+#a
+log(5 + (7^0.3))
+#b
+a = sqrt(3)
+atan(a)
+#c
+sin(tan(pi/2))
+#d
+factorial(23)
+
+#4
+demo(image()) # Exibe algums demonstracoes de scripts do R
+
+#5
+a.c<-1
+b.c<-2
+c.c<-3
+d.c<-4
+
+rm(list = ls(pattern = ".c")) 
+#6
+?q
+
+#7
+q()
