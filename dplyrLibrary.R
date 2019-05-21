78b66ae1c115321181edc385893066e3a7d6d3f6
Italo Lemos <italo_vinicius13@yahoo.com.br>
2019-05-21 19:36:34 -0300

    Some questions

--- dplyrLibrary.R
+++ dplyrLibrary.R
@@ -42,6 +42,62 @@ group_by(ChickWeight, Diet)
 
 str(ChickWeight)
 
+#Exercicio
+cafeina = read.table("cafeina.txt", header = T, sep = "", dec = ".")
 
-head(temp_by_moth)
-View(airquality)
+View(cafeina)
+str(cafeina)
+summary(cafeina)
+
+#media de idade para cada sexo
+cafeina %>%
+  group_by(gender) %>%
+  summarise(
+    mediaGenero = mean(age)
+  )
+
+# idade media para cada bebida
+cafeina %>%
+  group_by(drink) %>%
+  summarise(
+    mediaAge = mean(age)
+  )
+
+# idade media para cada nivel combinado de genero e bebida
+cafeina %>%
+  group_by(gender, drink) %>%
+  summarise(
+    mediaAge = mean(age)
+  )
+
+# calcular a pontuacao para cada idade
+cafeina %>% 
+  group_by(age) %>%
+  summarise(
+    pontuacao = median(score)
+  )
+
+# pontuacao maxima para cada idade apenas para homens
+cafeina %>%
+  group_by(age) %>%
+  filter(gender == "male") %>%
+  summarise(
+    pontMax = max(score)
+  )
+
+cafeina %>%
+  group_by(drink) %>%
+  summarise(
+    media = mean(score),
+    mediana = median(score),
+    desvio = sd(score)
+  )
+
+cafeina %>%
+  group_by(drink, cups) %>%
+  filter(gender == "female" & age > 20) %>%
+  summarise(
+    media = mean(score),
+    mediana = median(score),
+    desvio = sd(score)
+  )
